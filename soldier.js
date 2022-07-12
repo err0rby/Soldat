512f9685359d4f1097a2da5535e676697bb66cf6
errorby <mdidayev@inbox.ru>
2022-07-12 13:33:16 +0300

    Назвал свойства и методы солдата

---
+++ soldier.js
@@ -0,0 +1,18 @@
+const soldier = {
+    name: "Isa",
+    helth: 10,
+    gun: {
+        gunName: "Glock",
+        patron: 30
+    },
+    gunVal: 3,
+    fire: function () {
+
+    },
+    reval: function () {
+
+    },
+    murder: function () {
+
+    }
+};
