462d1b6c52a5161123e1a850ec640180cce39048
errorby <mdidayev@inbox.ru>
2022-07-12 14:07:50 +0300

    Написал методы для солдата

--- soldier.js
+++ soldier.js
@@ -7,12 +7,30 @@ const soldier = {
     },
     gunVal: 3,
     fire: function () {
-
+        this.gun.patron--;
+        if (this.gun.patron == 0) {
+            console.log ("Обойма пуста. Перезаредитесь.");
+        }else {
+            console.log ("бах-бах");
+        }
     },
     reval: function () {
-
+        this.gun.patron = 30;
+        this.gunVal--;
+        if (this.gunVal == 0) {
+        console.log ("Не осталось припасов");
+        }else {
+            console.log("Перезарядка...");
+        }
     },
     murder: function () {
-
+        this.helth--;
+        if (this.helth == 0) {
+            console.log ("Ты проиграл");
+        }
     }
 };
+soldier.fire();
+console.log(soldier.gun.patron);
+soldier.reval();
+console.log(soldier.gun.patron);
